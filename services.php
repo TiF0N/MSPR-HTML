6eb89299703847fe6c0011403fa0114fbdf153c5
Killian LE MOAL <killian.lemoal2001@gmail.com>
2020-02-03 16:59:51 +0100

    update: random

--- services.php
+++ services.php
@@ -51,14 +51,14 @@
     </section>
     <section id="container">
         <div class="pre-footer">
-            <h2>Intéressé</h2>
+            <h2>Respirez et détendez-vous !</h2>
             <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat faucibus est, eu
                 ullamcorper magna faucibus nec. Sed ac lorem ut augue consequat molestie non vel massa. Quisque ornare
                 fermentum enim. Aliquam sit amet eros tempor, volutpat quam ut, condimentum diam. Ut congue eros quis mi
                 mattis aliquam. Morbi vestibulum tortor accumsan, aliquet nunc a, porttitor nisl. Maecenas viverra purus
                 sagittis mauris faucibus, at pellentesque neque sollicitudin.</p>
             <button class="btn btn-primary">
-                Découvrir nos tarifs
+                Découvrir la ZenAttitude
             </button>
         </div>
     </section>
--- zenattitude.php
+++ zenattitude.php
@@ -1,9 +1,52 @@
 <?php include_once 'includes/header.php'; ?>
 
-.concept
     <section id="jumbotron">
         <div class="jumbotron">
-            <h1>Notre concept</h1>
+            <h1>La ZenAttitude</h1>
         </div>
-        <section>
+    </section>
+    <section id="container">
+        <div class="tarifs">
+            <h2>Adoptez la ZenAttitude !</h2>
+            <h3>Découvrez les ZenEvents</h3>
+            <div class="row">
+                <div class="col">
+                    <img src="">
+                    <p>Interdum et malesuada fames ac ante ipsum</p>
+                </div>
+                <div class="col">
+                    <img src="">
+                    <p>Interdum et malesuada fames ac ante ipsum</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <img src="">
+                    <p>Interdum et malesuada fames ac ante ipsum</p>
+                </div>
+                <div class="col">
+                    <img src="">
+                    <p>Interdum et malesuada fames ac ante ipsum</p>
+                </div>
+            </div>
+        </div>
+        <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat faucibus est, eu
+            ullamcorper magna faucibus nec. Sed ac lorem ut augue consequat molestie non vel massa. Quisque ornare
+            fermentum enim. Aliquam sit amet eros tempor, volutpat quam ut, condimentum diam. Ut congue eros quis mi
+            mattis aliquam. Morbi vestibulum tortor accumsan, aliquet nunc a, porttitor nisl. Maecenas viverra purus
+            sagittis mauris faucibus, at pellentesque neque sollicitudin.</p>
+    </section>
+    <section id="container">
+        <div class="pre-footer">
+            <h2>Intéressé ?</h2>
+            <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat faucibus est, eu
+                ullamcorper magna faucibus nec. Sed ac lorem ut augue consequat molestie non vel massa. Quisque ornare
+                fermentum enim. Aliquam sit amet eros tempor, volutpat quam ut, condimentum diam. Ut congue eros quis mi
+                mattis aliquam. Morbi vestibulum tortor accumsan, aliquet nunc a, porttitor nisl. Maecenas viverra purus
+                sagittis mauris faucibus, at pellentesque neque sollicitudin.</p>
+            <button class="btn btn-primary">
+                Nous contacter
+            </button>
+        </div>
+    </section>
 <?php include_once 'includes/footer.php'; ?>
