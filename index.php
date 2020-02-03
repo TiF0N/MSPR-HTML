aee023599e3d2fcc545c47d8395bb186f5f42af0
Killian LE MOAL <killian.lemoal2001@gmail.com>
2020-02-03 14:29:26 +0100

    update: home v2

--- index.php
+++ index.php
@@ -1,1 +1,178 @@
-hello world
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ZenOffice</title>
+</head>
+<body>
+<header>
+    <nav class="navbar">
+        <a class="navbar-brand" href="index.html"><i class="#"></i></a>
+        <div class="navbar">
+            <ul>
+                <li>
+                    <a class="nav-links" href="#">Accueil</a>
+                </li>
+                <li>
+                    <a class="nav-links" href="#">Concept</a>
+                </li>
+                <li>
+                    <a class="nav-links" href="#">Services</a>
+                </li>
+                <li>
+                    <a class="nav-links" href="#">ZenAttitude</a>
+                </li>
+                <li>
+                    <a class="nav-links" href="#">Tarifs</a>
+                </li>
+                <li>
+                    <a class="nav-links" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<main id="main">
+    <section id="jumbotron">
+        <div class="jumbotron">
+            <h1>ZenOffice : Travaillez en vous relaxant !</h1>
+        </div>
+    </section>
+    <section id="container">
+        <div class="concept">
+            <h2>Notre concept</h2>
+            <p>Wafer caramels jujubes lemon drops donut pastry liquorice. Wafer pie caramels cake pastry candy canes
+                liquorice tart wafer. Biscuit fruitcake lollipop gummi bears. Dragée sesame snaps topping powder
+                pudding. Chocolate biscuit icing cake carrot cake lollipop caramels chocolate pastry. Bonbon caramels
+                bear claw jujubes powder. Muffin danish topping halvah dessert lemon drops jujubes chocolate soufflé.
+                Dragée powder brownie apple pie gummies chocolate chupa chups. Candy canes brownie gummies gummies
+                macaroon tootsie roll sugar plum. Halvah soufflé marshmallow tiramisu lemon drops. Lollipop gummies
+                gummi bears marzipan. Chocolate bar carrot cake cake candy halvah danish.</p>
+        </div>
+        <button class="btn btn-primary">Découvrir notre concept</button>
+    </section>
+    <section id="container>">
+        <div class="services">
+            <div class="col">
+                <div class="row">
+                    <h2>Zone Zen (SPA)</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor nisi, consectetur eu eleifend
+                        sit amet, fermentum at ex. Integer non eleifend lorem, vitae placerat ipsum. Pellentesque ornare
+                        lectus quis laoreet pulvinar. Ut pellentesque ornare tellus. Quisque lobortis scelerisque
+                        aliquet. Proin ut sem a nunc finibus vehicula nec sed diam. Proin sed cursus nunc. Vestibulum
+                        lectus velit, faucibus non pulvinar sed, porttitor sit amet enim. Nulla luctus sem eu magna
+                        fermentum fermentum. Vestibulum ut dui non ipsum venenatis condimentum.</p>
+                </div>
+                <div class="col">
+                    <div class="row">
+                        <img src="#">
+                    </div>
+                    <div class="col">
+                        <div class="row">
+                            <img src="#">
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <h2>Restaurant Zenitude</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor nisi, consectetur
+                                    eu eleifend sit amet, fermentum at ex. Integer non eleifend lorem, vitae placerat
+                                    ipsum. Pellentesque ornare lectus quis laoreet pulvinar. Ut pellentesque ornare
+                                    tellus. Quisque lobortis scelerisque aliquet. Proin ut sem a nunc finibus vehicula
+                                    nec sed diam. Proin sed cursus nunc. Vestibulum lectus velit, faucibus non pulvinar
+                                    sed, porttitor sit amet enim. Nulla luctus sem eu magna fermentum fermentum.
+                                    Vestibulum ut dui non ipsum venenatis condimentum.</p>
+                            </div>
+                            <div class="col">
+                                <div class="row">
+                                    <h2>Zone Zen (SPA)</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor nisi,
+                                        consectetur eu eleifend sit amet, fermentum at ex. Integer non eleifend lorem,
+                                        vitae placerat ipsum. Pellentesque ornare lectus quis laoreet pulvinar. Ut
+                                        pellentesque ornare tellus. Quisque lobortis scelerisque aliquet. Proin ut sem a
+                                        nunc finibus vehicula nec sed diam. Proin sed cursus nunc. Vestibulum lectus
+                                        velit, faucibus non pulvinar sed, porttitor sit amet enim. Nulla luctus sem eu
+                                        magna fermentum fermentum. Vestibulum ut dui non ipsum venenatis
+                                        condimentum.</p>
+                                </div>
+                                <div class="col">
+                                    <div class="row">
+                                        <img src="#">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary">Découvrir nos services</button>
+    </section>
+    <section id="container">
+        <div class="zenattitude">
+            <h2>La ZenAttitude</h2>
+            <p>Aenean quam odio, pulvinar id ornare eu, tempus sed nulla. Suspendisse molestie vulputate sem, tincidunt
+                posuere erat ullamcorper sit amet. Integer lacinia lorem neque, ac iaculis augue faucibus non. Duis
+                malesuada maximus diam, ac semper tellus. Ut mattis massa dui, in lacinia velit mattis ut. Aenean sed
+                posuere metus. Duis finibus iaculis purus, ac interdum mi rhoncus id. Proin dapibus, neque id dignissim
+                pharetra, arcu nunc convallis tortor, ac auctor enim neque in sem. Sed finibus suscipit elementum. Donec
+                nec massa nibh. Morbi quis eros massa. Integer ut quam vel augue condimentum molestie et eget quam.
+                Maecenas ipsum nisl, feugiat quis eleifend sed, semper id lectus. Nullam fermentum lobortis rhoncus.</p>
+        </div>
+        <div class="col">
+            <img src="#">
+        </div>
+        <div class="col">
+            <img src="#">
+        </div>
+        <div class="col">
+            <img src="#">
+        </div>
+        <button class="btn btn-primary">
+            Les ZenEvents
+        </button>
+    </section>
+    <section id="container">
+        <div class="tarifs">
+            <div class="col">
+                <img src="#">
+            </div>
+            <div class="col">
+                <img src="#">
+            </div>
+        </div>
+        <button class="btn btn-primary">
+            Obtenir plus de détail
+        </button>
+    </section>
+    <footer>
+        <div class="row">
+            <div class="col">
+                <h4>Plan du site</h4>
+                <ul>
+                    <li>
+                        Accueil
+                    </li>
+                    <li>
+                        Concept
+                    </li>
+                    <li>
+                        Service
+                    </li>
+                    <li>
+                        ZenAttitude
+                    </li>
+                    <li>
+                        Tarifs
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                </ul>
+                <div class="col">
+                    <p>Ouvert du lundi au vendredi de 7h à 22h</p>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
