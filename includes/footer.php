b6d770bdd63c33965b5f5c034467386ef8531087
Rémy <remyguillet44@gmail.com>
2020-02-04 12:15:25 +0100

    update: footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,54 +1,68 @@
 </main>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
+      integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous"/>
 <footer>
     <div class="row">
         <div class="col">
-            <h4>Plan du site</h4>
-            <ul>
-                <li>
-                    Accueil
-                </li>
-                <li>
-                    Concept
-                </li>
-                <li>
-                    Service
-                </li>
-                <li>
-                    ZenAttitude
-                </li>
-                <li>
-                    Tarifs
-                </li>
-                <li>
-                    Contact
-                </li>
-            </ul>
-            <div class="col">
-                <p>Ouvert du lundi au vendredi de 7h à 22h</p>
-                <ul>
-                    <li>
-                        Adresse
-                    </li>
-                    <li>
-                        Téléphone
-                    </li>
-                    <li>
-                        Email
-                    </li>
-                </ul>
-            </div>
+            <table class="table" summary="">
+
         </div>
         <div class="google_maps">
             <card></card>
         </div>
     </div>
-    <div class="row">
-        <div class="col">
-            <img src="public/images/logo fb blanc.png">
-            <img src="public/images/logo_linkedin.png">
-            <i class="fab fa-facebook"></i>
-        </div>
-    </div>
+
 </footer>
 </body>
 </html>
+<!-- Site footer -->
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="site">
+                <h6>Nous trouver</h6>
+
+            </div>
+
+            <div class="container">
+                <h6 class="plan">Plan du site</h6>
+                <ul class="footer-links">
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="concept.php">Concept</a></li>
+                    <li><a href="services.php">Services</a></li>
+                    <li><a href="zenattitude.php">ZenAttitude</a></li>
+                    <li><a href="tarifs.php">Tarifs</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="contact">
+                <h6 class="plan">Nous contacter</h6>
+                <ul class="footer-find">
+                    <li>Ouvert du lundi au vendredi de 7h à 22h</li>
+                    <li>Adresse : Île de Versailles 44000 Nantes</li>
+                    <li>Téléphone : 07.36.16.62.46</li>
+                    <li>Email : contact@zenoffice.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div>
+                <ul class="social-icons">
+                    <li><a> <i class="fab fa-facebook"></i></a></li>
+                    <li><a> <i class="fab fa-instagram"></i></a></li>
+                    <li><a> <i class="fab fa-linkedin-in"></i></a></li>
+                </ul>
+            </div>
+            <div class="copyright">
+                <p>Copyright &copy; 2020
+                    <a href="#">ZenOffice</a>.
+                </p>
+            </div>
+
+        </div>
+    </div>
+</footer>
