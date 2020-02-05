7442203c215c8532db4832cd8c0ea594cfb85947
Killian LE MOAL <killian.lemoal2001@gmail.com>
2020-02-05 12:39:52 +0100

    update: zenattitude

--- zenattitude.php
+++ zenattitude.php
@@ -9,32 +9,34 @@
         <div class="container">
             <h2>Adoptez la ZenAttitude !</h2>
             <h3>Découvrez les ZenEvents</h3>
-            <div class="row">
-                <div class="col">
+            <div class="grid-2">
+                <div>
                     <img src="public/images/bamboo.png">
                     <p>Découverte de son Ikigaï</p>
                 </div>
-                <div class="col">
-                    <img src="">
+                <div>
+                    <img src="public/images/respiration.jpg">
                     <p>Relaxation par la respiration</p>
                 </div>
             </div>
-            <div class="row">
-                <div class="col">
+
+            <div class="grid-2">
+                <div>
                     <img src="public/images/palais_jardin.png">
                     <p>Séparer vie personnelle et et vie professionnelle</p>
                 </div>
-                <div class="col">
+                <div>
                     <img src="public/images/jardin.png">
                     <p>Apprendre tout en innovant</p>
                 </div>
             </div>
+
+            <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat faucibus est, eu
+                ullamcorper magna faucibus nec. Sed ac lorem ut augue consequat molestie non vel massa. Quisque ornare
+                fermentum enim. Aliquam sit amet eros tempor, volutpat quam ut, condimentum diam. Ut congue eros quis mi
+                mattis aliquam. Morbi vestibulum tortor accumsan, aliquet nunc a, porttitor nisl. Maecenas viverra purus
+                sagittis mauris faucibus, at pellentesque neque sollicitudin.</p>
         </div>
-        <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat faucibus est, eu
-            ullamcorper magna faucibus nec. Sed ac lorem ut augue consequat molestie non vel massa. Quisque ornare
-            fermentum enim. Aliquam sit amet eros tempor, volutpat quam ut, condimentum diam. Ut congue eros quis mi
-            mattis aliquam. Morbi vestibulum tortor accumsan, aliquet nunc a, porttitor nisl. Maecenas viverra purus
-            sagittis mauris faucibus, at pellentesque neque sollicitudin.</p>
     </section>
     <section id="pro-footer">
         <div class="container">
